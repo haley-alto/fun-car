513cf4c396b47b23735ab6ffda6dc90a2b7d3381
haleydemos <haley.demos@ridealto.com>
2022-10-28 15:50:48 -0500

    works with vite

--- app.js
+++ app.js
@@ -1,80 +1,41 @@
-// // Option 1: Import the entire three.js core library.
-// import * as THREE from 'three';
+// import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.146.0/three.min.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const scene = new THREE.Scene();
+console.log(scene);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-// // const scene = new THREE.Scene();
-// // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new GLTFLoader();
+let car;
+// const loader = new GLTFLoader();
+loader.load('shelby-cobra-427/source/shelby-cobra-427.glb', function ( gltf ) {
 
-// // const renderer = new THREE.WebGLRenderer();
-// // renderer.setSize( window.innerWidth, window.innerHeight );
-// // document.body.appendChild( renderer.domElement );
+    car = gltf.scene;  // sword 3D object is loaded
+    car.scale.set(2, 2, 2);
+    car.position.y = 4;
+    scene.add(car);
+}, undefined, function ( error ) {
+    console.error( error );
+} );
 
-// // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// // const cube = new THREE.Mesh( geometry, material );
-// // scene.add( cube );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// // camera.position.z = 5;
+// var geometry =  new THREE.BoxGeometry(3, 1, 3, 10, 10, 10);
+// var material = new THREE.MeshBasicMaterial( {
+// 	opacity: 0.5,
+// 	transparent: true,
+// 	color: 0xA765C9,
+// 	wireframe: true,
+// });
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
-// // function animate() {
-// //     requestAnimationFrame( animate );
+camera.position.z = 1;
 
-// //     cube.rotation.x += 0.01;
-// //     cube.rotation.y += 0.01;
-
-// //     renderer.render( scene, camera );
-// // };
-
-// // animate();
-
-// //this.animate(null);
-// scene = new THREE.Scene();
-// camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// boxGeometry = new THREE.BoxGeometry(2, 2, 2);
-// material = new THREE.MeshBasicMaterial({color: 0xff0000});
-// mesh = new THREE.Mesh( boxGeometry, material );
-//   canva = document.getElementById("canvas");
-//      renderer = new THREE.WebGLRenderer({antialias: true, canvas: canva });
-
-// startScene();
-// animate();
-
-// function startScene() {
-
-//   //let scene = new THREE.Scene();
-//   //let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  
-//   let light = new THREE.AmbientLight( 0xffaaff ); 
- 
-
-//     scene.add( mesh );
-//     light.position.set(10,10,10)
-//     scene.add( light );
-//     camera.position.set(0, 0, 5);
-//     mesh.castShadow = true;
-//     mesh.receiveShadow = true;
-    
-//     //this.renderer.setClearColor('#E5E5E5')
-//     renderer.setClearColor(0xEEEEEE);
-//     //this.renderer.setSize(window.innerWidth, window.innerHeight)
-//     //this.renderer.render(this.scene, this.camera)
-//     //this.renderer.setAnimationLoop(this.animate.bind(this))
-//     //document.body.appendChild(this.renderer.domElement)
-
-//     // window.addEventListener('resize', () => {
-//     //   this.renderer.setSize(window.innerWidth, window.innerHeight)
-//     //   this.camera.aspect = window.innerWidth / innerHeight
-//     //   this.camera.updateProjectionMatrix();
-//     // })
-// } 
-
-// function animate() {
-//  camera.aspect = window.innerWidth / innerHeight
-//     camera.updateProjectionMatrix();
-//     requestAnimationFrame( animate );
-
-//     mesh.rotation.x += 0.01;
-//     //this.mesh.rotation.y += 1;
-//     //document.body.appendChild(this.renderer.domElement)
-
-//     renderer.render(scene, camera);
-// }
+var animate = function () {
+    requestAnimationFrame( animate );
+    // cube.rotation.y += -0.005
+    renderer.render( scene, camera );
+};
