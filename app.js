0374c95bcbf7479ca489f8e082415785672253c2
haleydemos <haley.demos@ridealto.com>
2022-10-28 19:29:56 -0500

    working checkers board with orbit

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
-// import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.146.0/three.min.js';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-const scene = new THREE.Scene();
-console.log(scene);
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// import { GLTFLoader} from 'https://unpkg.com/three@0.146.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// const hlight = new AmbientLight (0x404040,100);
+// const container = document.querySelector('#scene-container');
+var scene, camera, renderer, cube, controls;
 
-const loader = new GLTFLoader();
-let car;
-// const loader = new GLTFLoader();
-loader.load('shelby-cobra-427/source/shelby-cobra-427.glb', function ( gltf ) {
+init();
 
-    car = gltf.scene;  // sword 3D object is loaded
-    car.scale.set(2, 2, 2);
-    car.position.y = 4;
-    scene.add(car);
-}, undefined, function ( error ) {
-    console.error( error );
-} );
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-// var geometry =  new THREE.BoxGeometry(3, 1, 3, 10, 10, 10);
-// var material = new THREE.MeshBasicMaterial( {
-// 	opacity: 0.5,
-// 	transparent: true,
-// 	color: 0xA765C9,
-// 	wireframe: true,
-// });
-// var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+    const square = new THREE.BoxGeometry(1,0.1,1);
+    const lightSquare = new THREE.MeshBasicMaterial({ color: 0xE0C4A8});
+    const darkSquare = new THREE.MeshBasicMaterial({ color: 0x6A4236});
 
-camera.position.z = 1;
+    let board = new THREE.Group();
 
-var animate = function () {
-    requestAnimationFrame( animate );
-    // cube.rotation.y += -0.005
+    for (let x = 1; x <= 10; x++) {
+        for (let z = 1; z <= 10; z++){
+            if(z % 2 === 0) {
+                cube = new THREE.Mesh(square, x % 2 === 0 ? lightSquare : darkSquare);
+            } else {
+                cube = new THREE.Mesh(square, x % 2 === 0 ? darkSquare : lightSquare);
+            }
+            cube.position.set(x,0,z);
+            board.add(cube);
+        }
+    }
+
+    scene.add(board);
+
+    camera.position.y = 1;
+    camera.position.z = 3;
+    // camera.lookAt(0,0,0);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.target.set(4.5,0,4.5); //this sets the camera on oribit controls
+    controls.enablePan = false; //horitonzal scrolling
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.enableDamping = true;
+
+    // controls.update();
+
+    window.requestAnimationFrame(animate);
+}
+
+function animate() {
+    controls.update();
     renderer.render( scene, camera );
-};
+    window.requestAnimationFrame( animate );
+}
